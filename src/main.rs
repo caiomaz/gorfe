e0defdbe186b68075b92f583516d3f212e7d34ee
caiomaz <caiotmzferreira@gmail.com>
2025-01-12 14:36:22 -0300

    Change addr

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() {
     let app: Router = routes::create_router();
 
     // Define the address and port for the server
-    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 4000));
+    let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], 4000));
     println!("Server running at http://{}", addr);
 
     // Bind the server to the address and serve the application
